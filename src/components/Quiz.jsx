9fe29d2c3c8b75dc37ba52c0ea46eb4f55e59cdd
bsplabs <bsplabs@gmail.com>
2025-01-20 13:48:30 +0700

    outputting quiz results

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,9 @@
 import { useState, useCallback } from 'react';
 
 import Question from './Question.jsx';
+import Summary from './Summary.jsx';
 
 import QUESTIONS from '../questions.js';
-import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
   console.log('Quiz render');
@@ -28,12 +28,7 @@ export default function Quiz() {
   );
 
   if (quizIsComplete) {
-    return (
-      <div id="summary">
-        <img src={quizCompleteImg} alt="Tropy icon" />
-        <h2>Quiz Completed!</h2>
-      </div>
-    );
+    return <Summary userAnswers={userAnswers} />;
   }
 
   return (
